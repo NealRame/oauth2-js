973301fb197c11d5bbe8f590cc64fcb44e2a1481
NealRame <contact@nealrame.com>
2014-08-09 02:20:17 +0200

    Update 'test/app.js'

--- test/app.js
+++ test/app.js
@@ -28,26 +28,21 @@ var oauth2_params = JSON.parse(fs.readFileSync(path.join(__dirname, 'oauth2.json
 /// Define User helpers here.
 var oauth2_helpers = {
     /// ### `find_user(id, callback)`
-    /// Asynchronously associate an instance of your _User_ _model_ to a
-    /// given _Open ID_ [_successful token response_](http://openid.net/specs/openid-connect-core-1_0.html#TokenResponse).
+    /// Asynchronously associate an instance of your _User_ _model_ to a given
+    /// _Open ID_ [_token_id_](http://openid.net/specs/openid-connect-core-1_0.html#IDToken).
     ///
     /// _Parameters_:
-    /// * `token`
-    ///   An [Open ID successful token response]. According to how you have
+    /// * `token_id`,
+    ///   An _Open ID token_id_. According to how you have
     ///   configured your authentication request scope parameter, this is a
     ///   possible token value:
     ///     ```javascript
-    ///     {
-    ///         access_token: 'SlAV32hkKG',
-    ///         token_type: 'Bearer',
-    ///         expires_in: 3600,
-    ///         id_token: {
-    ///             iss: 'URL',           // REQUIRED
-    ///             sub: '[0-9a-zA-Z]+',  // REQUIRED
-    ///             aud: '...',           // REQUIRED
-    ///             exp: 1407534254989,   // REQUIRED
-    ///             iat: 1407534288627,   // REQUIRED
-    ///         }
+    ///     id_token: {
+    ///         iss: 'URL',           // REQUIRED
+    ///         sub: '[0-9a-zA-Z]+',  // REQUIRED
+    ///         aud: '...',           // REQUIRED
+    ///         exp: 1407534254989,   // REQUIRED
+    ///         iat: 1407534288627,   // REQUIRED
     ///     }
     ///     ```
     ///
@@ -57,23 +52,17 @@ var oauth2_helpers = {
     ///   - The first argument is always reserved for an exception.
     ///     If the operation was completed successfully, then the first
     ///     argument will be `null` or `undefined`.
-    ///   - The second parameter is a hash object containing the following
-    ///     attribute:
-    ///     ```javascript
-    ///     {
-    ///       token: { ... } // the passed token argument
-    ///       user:  { ... } // the matching instance of your _User_ _model_
-    ///     }
-    ///     ```
-    find_user: function(token, callback) {
-        callback(null, users[token.id_token.sub]);
+    ///   - The second parameter is an instance of your _User_ _model_
+    find_user: function(id, callback) {
+        callback(null, users[id_token.sub]);
     },
     /// ### `is_initialized(user)`
     /// Unsynchronously check if the given user is considered _initialized_
     /// regarding to your _User_ _model_ state.
     ///
     /// _Parameters_:
-    /// * `params`,
+    /// * `id`,
+    ///   A token_id
     ///   A hash object containing the following attributes:
     ///   - **`token`**, see `find_user()` above for more details,
     ///   - `user`, an instance of your _User_ _model_.
@@ -86,16 +75,9 @@ var oauth2_helpers = {
     ///     argument will be `null` or `undefined`.
     ///   - The second parameter of the callback is a boolean value, set to
     ///     `true` if and only if the user is considered initialized.
-    ///   - The third parameter is a hash object containing the following
-    ///     attribute:
-    ///     ```javascript
-    ///     {
-    ///       token: { ... } // the passed token argument
-    ///       user:  { ... } // the matching instance of your _User_ _model_
-    ///     }
-    ///     ```
-    is_initialized: function(params, callback) {
-        return user.initialized;
+    ///   - The third parameter is the passed instance of your _User_ _model_.
+    is_initialized: function(user, callback) {
+        callback(null, user.initialized, user);
     },
     /// ### `map_oauth2_user_info(oauth2_user_info, callback)`
     /// Unsynchronously map the _ Open ID_ [_UserInfo_](http://openid.net/specs/openid-connect-core-1_0.html#UserInfo)
@@ -116,23 +98,15 @@ var oauth2_helpers = {
     ///   - The first argument is always reserved for an exception.
     ///     If the operation was completed successfully, then the first
     ///     argument will be `null` or `undefined`.
-    ///   - The second parameter is a hash object containing the following
-    ///     attribute:
-    ///     ```javascript
-    ///     {
-    ///       token: { ... } // the passed token argument
-    ///       user:  { ... } // the matching instance of your _User_ _model_
-    ///     }
-    ///     ```
-    map_oauth2_user_info: function(params, oauth2_user_info, callback) {
-        var user = {
-            name: {
-                first: oauth2_user_info.given_name,
-                last: oauth2_user_info.family_name
-            },
-            email: oauth2_user_info.email,
-            picture: oauth2_user_info.picture
+    ///   - The third parameter is the passed instance of your _User_ _model_.
+    map_oauth2_user_info: function(user, oauth2_user_info, callback) {
+        user.name = {
+            first: oauth2_user_info.given_name,
+            last: oauth2_user_info.family_name
         };
+        user.email = oauth2_user_info.email;
+        user.picture = oauth2_user_info.picture;
+
         callback(null, user);
     }
 };
